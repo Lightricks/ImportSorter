c910ae1c53f1f7e10bfaf136fd0bf52d25b0d9f0
Barak Weiss <barakwei@gmail.com>
2016-06-22 10:56:02 +0300

    bump to 1.0.3, with LT changes

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-VERSION=1.0.2
-DOWNLOAD_URI="http://github.com/manji602/ImportSorter/releases/download/${VERSION}/ImportSorter.tar.gz"
+VERSION=1.0.3
+DOWNLOAD_URI="http://github.com/Lightricks/ImportSorter/releases/download/${VERSION}/ImportSorter.tar.gz"
 PLUGINS_DIR="${HOME}/Library/Application Support/Developer/Shared/Xcode/Plug-ins"
 
 mkdir -p "${PLUGINS_DIR}"
