82c3d3e9e416bb8df584bed95d94df214721b3c4
Ben Yohay <benyohay91@gmail.com>
2016-08-02 14:26:40 +0300

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=1.0.4
+VERSION=1.0.6
 DOWNLOAD_URI="http://github.com/Lightricks/ImportSorter/releases/download/${VERSION}/ImportSorter.tar.gz"
 PLUGINS_DIR="${HOME}/Library/Application Support/Developer/Shared/Xcode/Plug-ins"
 
